2e09f849c34393cce832bb15906598ff35c1b192
posiputt <i@posiputt.de>
2015-09-26 00:10:29 +0200

    completed study drills ex3-5

--- ex3.py
+++ ex3.py
@@ -3,10 +3,10 @@ print "I will now count my chickens:"
 
 # print the word "Hens" followed by the result of
 # 25 + 30 / 6 (which is 25 + 5 = 30)
-print "Hens", 25 + 30 / 6
+print "Hens", 25.0 + 30.0 / 6.0
 # print the word "Roosters" followed by the result of
 # 100 - 25 * 3 % 4 (which is 100 - 75 % 4 -> 100 - 3 = 97)
-print "Roosters", 100 - 25 * 3 % 4
+print "Roosters", 100.0 - 25.0 * 3.0 % 4.0
 
 # print the sentence "Now I will count the eggs:"
 print "Now I will count the eggs:"
@@ -14,7 +14,7 @@ print "Now I will count the eggs:"
 # print the result of
 # 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
 # which is 1 + 0 - 0 + 6 = 7
-print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
 
 # print the question "Is it true that 3 + 2 < 5 - 7?"
 print "Is it true that 3 + 2 < 5 - 7?"
@@ -22,14 +22,14 @@ print "Is it true that 3 + 2 < 5 - 7?"
 # print the result of
 # 3 + 2 < 5 - 7
 # which is 5 < -2 = False
-print 3 + 2 < 5 - 7
+print 3.0 + 2.0 < 5.0 - 7.0
 
 # print the question "What is 3 - 2?" followed by the result of
 # 3 + 2 (which is 5)
-print "What is 3 + 2?", 3 + 2
+print "What is 3 + 2?", 3.0 + 2.0
 # print the question "What is 5 - 7?" followed by the result of
 # 5 - 7 (which is -2)
-print "What is 5 - 7?", 5 - 7
+print "What is 5 - 7?", 5.0 - 7.0
 
 # print the sentence "Oh, that's why it's False."
 print "Oh, that's why it's False."
@@ -39,10 +39,10 @@ print "How about some more."
 
 # print the question "Is it greater?" followed by the result of
 # 5 > -2 (which is True)
-print "Is it greater?", 5 > -2
+print "Is it greater?", 5.0 > -2.0
 # print the question "Is it greater or equal?" followed by the result of
 # 5 >= -2 (which is True)
-print "Is it greater or equal?", 5 >= -2
+print "Is it greater or equal?", 5.0 >= -2.0
 # print the question "Is it less or equal?" followed by the result of
 # 5 <= -2 (which is False)
-print "Is it less or equal?", 5 <= -2
+print "Is it less or equal?", 5.0 <= -2.0
