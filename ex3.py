e12b293acf54adb34fbf40b5f6962bbb735397fa
posiputt <i@posiputt.de>
2015-09-26 00:01:28 +0200

    completed study drills ex3-1

--- ex3.py
+++ ex3.py
@@ -1,23 +1,48 @@
+# print the sentence "I will now count my chickens:"
 print "I will now count my chickens:"
 
+# print the word "Hens" followed by the result of
+# 25 + 30 / 6 (which is 25 + 5 = 30)
 print "Hens", 25 + 30 / 6
+# print the word "Roosters" followed by the result of
+# 100 - 25 * 3 % 4 (which is 100 - 75 % 4 -> 100 - 3 = 97)
 print "Roosters", 100 - 25 * 3 % 4
 
+# print the sentence "Now I will count the eggs:"
 print "Now I will count the eggs:"
 
+# print the result of
+# 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+# which is 1 + 0 - 0 + 6 = 7
 print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
 
+# print the question "Is it true that 3 + 2 < 5 - 7?"
 print "Is it true that 3 + 2 < 5 - 7?"
 
+# print the result of
+# 3 + 2 < 5 - 7
+# which is 5 < -2 = False
 print 3 + 2 < 5 - 7
 
+# print the question "What is 3 - 2?" followed by the result of
+# 3 + 2 (which is 5)
 print "What is 3 + 2?", 3 + 2
+# print the question "What is 5 - 7?" followed by the result of
+# 5 - 7 (which is -2)
 print "What is 5 - 7?", 5 - 7
 
+# print the sentence "Oh, that's why it's False."
 print "Oh, that's why it's False."
 
+# print the sentence "How about some more."
 print "How about some more."
 
+# print the question "Is it greater?" followed by the result of
+# 5 > -2 (which is True)
 print "Is it greater?", 5 > -2
+# print the question "Is it greater or equal?" followed by the result of
+# 5 >= -2 (which is True)
 print "Is it greater or equal?", 5 >= -2
+# print the question "Is it less or equal?" followed by the result of
+# 5 <= -2 (which is False)
 print "Is it less or equal?", 5 <= -2
