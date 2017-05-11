9b4b783888ebdbb35f7f37f2b9c40157b050a9ff
posiputt <i@posiputt.de>
2017-05-11 16:51:36 +0200

    completed ex33 study drills 1 and 2

--- ex31-40/ex33.py
+++ ex31-40/ex33.py
@@ -1,15 +1,32 @@
-i = 0
-numbers = []
+def makeList(threshold):
+    i = 0
+    numbers = []
 
-while i < threshold:
-    print "At the top i is %d" % i
-    numbers.append(i)
+    while i < threshold:
+        print "At the top i is %d" % i
+        numbers.append(i)
 
-    i = i + 1
-    print "Numbers now: ", numbers
-    print "At the bottom i is %d" % i
+        i = i + 1
+        print "Numbers now: ", numbers
+        print "At the bottom i is %d" % i
 
-print "The numbers: "
+    print "The numbers: "
 
-for num in numbers:
-    print num
+    for num in numbers:
+        print num
+
+listSize = 0
+
+print "Hey... you wanna buy a list?"
+
+buyAList = raw_input("> ").lower()
+
+if buyAList == "yes":
+    print "How big should it be?"
+    listSize = int(raw_input("> "))
+    if listSize < 2:
+        exit("That's not a list!")
+else:
+    exit("Suit yourself.")
+
+makeList(listSize)
