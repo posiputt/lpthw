d5a52fba97f4d5a09df4059eb099d04d3d665b89
posiputt <i@posiputt.de>
2017-05-11 17:03:58 +0200

    completed ex33 study drill 3 and 4

--- ex31-40/ex33.py
+++ ex31-40/ex33.py
@@ -1,4 +1,4 @@
-def makeList(threshold):
+def makeList(threshold, increment):
     i = 0
     numbers = []
 
@@ -6,7 +6,7 @@ def makeList(threshold):
         print "At the top i is %d" % i
         numbers.append(i)
 
-        i = i + 1
+        i = i + increment
         print "Numbers now: ", numbers
         print "At the bottom i is %d" % i
 
@@ -16,17 +16,20 @@ def makeList(threshold):
         print num
 
 listSize = 0
+increment = 1
 
 print "Hey... you wanna buy a list?"
 
 buyAList = raw_input("> ").lower()
 
 if buyAList == "yes":
-    print "How big should it be?"
+    print "How big should the biggest number be?"
     listSize = int(raw_input("> "))
     if listSize < 2:
         exit("That's not a list!")
+    print "By how much should I increment between elements?"
+    increment = int(raw_input("> "))
 else:
     exit("Suit yourself.")
 
-makeList(listSize)
+makeList(listSize, increment)
